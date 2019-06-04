8b2733014a6f013db9e9b06bc1c2c6d15b8b206f
George Connor <grgcnnr@gmail.com>
2019-06-05 11:39:58 +1200

    Add example of wrap-reverse not uunsetting when using object values for reverse

--- example/components/Pages/Grid/Reverse.jsx
+++ example/components/Pages/Grid/Reverse.jsx
@@ -18,12 +18,21 @@ const normal = `
   <Grid.Unit size={0.25}>#4</Grid.Unit>
 </Grid>
 `;
+const object = `
+<Grid reverse={{mobile: false, tablet: true, desktop:false}}>
+  <Grid.Unit size={{mobile: 1/4, tablet: 1/2, desktop:1}}>#1</Grid.Unit>
+  <Grid.Unit size={{mobile: 1/4, tablet: 1/2, desktop:1}}>#2</Grid.Unit>
+  <Grid.Unit size={{mobile: 1/4, tablet: 1/2, desktop:1}}>#3</Grid.Unit>
+  <Grid.Unit size={{mobile: 1/4, tablet: 1/2, desktop:1}}>#4</Grid.Unit>
+</Grid>
+`;
 
 const Reverse = () => (
   <Component name="">
     <Property name="reverse">
       <Value name="false" example={normal} />
       <Value name="true" example={reverse} />
+      <Value name="object" example={object} />
     </Property>
   </Component>
 );
